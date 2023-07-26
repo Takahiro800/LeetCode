0c8b7b57aaed80b81894a9903b9ed7b5d7cc7ebb
taemath <hirotaka081630@gmail.com>
2023-07-27 04:38:55 +0900

    feat(ruby/217): array

--- ruby/217.contains-duplicate.rb
+++ ruby/217.contains-duplicate.rb
@@ -1,13 +1,12 @@
 # @param {Integer[]} nums
 # @return {Boolean}
+
+require "set"
+
 def contains_duplicate(nums)
-  hashset = {}
-
-  nums.each do |num|
-    return true if hashset.key?(num)
-
-    hashset[num] = true
-  end
-
-  false
+  nums.each_with_object(Set.new) do |num, set|
+    return true unless set.add?(num)
+  end and false
 end
+
+puts contains_duplicate([])
