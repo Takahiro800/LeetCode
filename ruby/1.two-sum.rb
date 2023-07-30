743daf85d8352ae41c1a5798009c475df2854012
taemath <hirotaka081630@gmail.com>
2023-07-31 04:33:51 +0900

    refactor(ruby/1): use each_with_object

--- ruby/1.two-sum.rb
+++ ruby/1.two-sum.rb
@@ -2,11 +2,9 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-  hash = {}
+  nums.each_with_object({}).each_with_index do |(n, hash), i|
+    return [hash[target - n], i] if hash[target - n]
 
-  nums.each_with_index do |num, i|
-    return [i, hash[target - num]] if hash[target - num]
-
-    hash[num] = i
+    hash[n] = i
   end
 end
