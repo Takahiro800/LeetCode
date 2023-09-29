ef297bd3ca6880cbee6790037eccdd7ad19b95b6
taemath <hirotaka081630@gmail.com>
2023-09-30 05:29:00 +0900

    refactor(ruby:125): fix

--- ruby/125.valid-palindrome.rb
+++ ruby/125.valid-palindrome.rb
@@ -2,7 +2,5 @@
 # @return {Boolean}
 def is_palindrome(s)
   processed_s = s.downcase.gsub(/[^a-z0-9]/, '')
-  revers_s = processed_s.reverse
-
-  processed_s == revers_s
+  processed_s == processed_s.reverse
 end
