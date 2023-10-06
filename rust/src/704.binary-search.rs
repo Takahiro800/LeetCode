70540eaa04178628c26e37fb2991b13bc56afb6d
taemath <hirotaka081630@gmail.com>
2023-10-06 17:19:20 +0900

    refactor(rust): 704.binary-search.rs

--- rust/src/704.binary-search.rs
+++ rust/src/704.binary-search.rs
@@ -1,19 +1,16 @@
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        use std::cmp::Ordering;
         let mut left = 0;
         let mut right = nums.len();
 
         while left < right {
-            let mid = left + (right - left) / 2;
+            let mid = (left + right) / 2;
 
-            if nums[mid] == target {
-                return mid as i32;
-            }
-
-            if target < nums[mid] {
-                right = mid;
-            } else {
-                left = mid + 1;
+            match nums[mid].cmp(&target) {
+                Ordering::Equal => return mid as i32,
+                Ordering::Less => left = mid + 1,
+                Ordering::Greater => right = mid,
             }
         }
 
