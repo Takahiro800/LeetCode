7c002672f967f393e04ce3112f78cb7c5570117a
taemath <hirotaka081630@gmail.com>
2023-10-12 05:55:20 +0900

    refactor(rust): 121.best-time-to-buy-and-sell-stock.rs

--- rust/src/121.best-time-to-buy-and-sell-stock.rs
+++ rust/src/121.best-time-to-buy-and-sell-stock.rs
@@ -1,22 +1,10 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-        let mut left = 0;
-        let mut right = 1;
-
-        let mut current_max = 0;
-
-        while right < prices.len() {
-            if prices[left] < prices[right] {
-                let profit = prices[right] - prices[left];
-                if profit > current_max {
-                    current_max = profit;
-                }
-            } else {
-                left = right;
-            }
-            right += 1;
-        }
-
-        current_max
+        prices
+            .iter()
+            .fold((i32::MAX, 0), |(min, max_profit), &el| {
+                (el.min(min), max_profit.max(el - min))
+            })
+            .1
     }
 }
