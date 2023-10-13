5361927280f8f9f3dbfae542fba860dcd9ab9809
taemath <hirotaka081630@gmail.com>
2023-10-14 05:42:54 +0900

    refactor(rust): 3.longest-substring-without-repeating-characters.rs

--- rust/src/3.longest-substring-without-repeating-characters.rs
+++ rust/src/3.longest-substring-without-repeating-characters.rs
@@ -1,22 +1,23 @@
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
         let mut max = 0;
-        let mut appear = std::collections::HashMap::new();
+        let mut appear = std::collections::HashSet::new();
+        let mut window: Vec<char> = vec![];
 
-        let arr: Vec<char> = s.chars().collect();
-        let mut left = 0;
-
-        for right in 0..arr.len() {
-            let char = arr[right];
-
-            while appear.contains_key(&char) {
-                appear.remove(&arr[left]);
-                left += 1;
+        for char in s.chars() {
+            while appear.contains(&char) {
+                if let Some(c) = window.get(0) {
+                    appear.remove(c);
+                    window.remove(0);
+                }
             }
-            appear.insert(char, right);
-            max = max.max(right - left + 1);
-        }
+            appear.insert(char);
+            window.push(char);
 
+            if window.len() > max {
+                max = window.len();
+            }
+        }
         max as i32
     }
 }
