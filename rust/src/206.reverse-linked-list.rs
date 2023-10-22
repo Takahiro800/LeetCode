b7d02fc0c13b6d8c0ecdf9c8a2fec715cc9141ea
taemath <hirotaka081630@gmail.com>
2023-10-23 03:58:00 +0900

    feat(rust): 206.reverse-linked-list.rs

---
+++ rust/src/206.reverse-linked-list.rs
@@ -0,0 +1,31 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut prev_node = None;
+        let mut curr_node = head;
+
+        while let Some(mut node) = curr_node {
+            let next_node = node.next.take();
+            node.next = prev_node;
+            prev_node = Some(node);
+            curr_node = next_node;
+        }
+
+        prev_node
+    }
+}
