58772f6c8bf8c63fdc59bd256c707e61d726077a
taemath <hirotaka081630@gmail.com>
2023-11-20 04:11:42 +0900

    refactor(ruby): 2.add-two-numbers.rs

--- ruby/2.add-two-numbers.rb
+++ ruby/2.add-two-numbers.rb
@@ -1,39 +1,17 @@
 def add_two_numbers(l1, l2)
-  # 必須。headを用意する
-  result_node = ListNode.new
-  head = result_node
+  head = ListNode.new(0)
+  current = head
   carry = 0
 
-  while l1 && l2
-    current_sum = l1.val + l2.val + carry
-    result_node.val = current_sum % 10
-    carry = current_sum / 10
+  while l1 || l2 || carry.positive?
+    sum = (l1&.val || 0) + (l2&.val || 0) + carry
+    carry = sum / 10
+    current.next = ListNode.new(sum % 10)
+    current = current.next
 
-    l1 = l1.next
-    l2 = l2.next
-
-    if l1 || l2 || carry.positive?
-      result_node.next = ListNode.new
-      result_node = result_node.next
-    end
+    l1 = l1&.next
+    l2 = l2&.next
   end
 
-  remainning = l1 || l2
-
-  while remainning
-    current_sum = remainning.val + carry
-    result_node.val = current_sum % 10
-    carry = current_sum / 10
-
-    remainning = remainning.next
-
-    if remainning || carry.positive?
-      result_node.next = ListNode.new
-      result_node = result_node.next
-    end
-  end
-
-  result_node.val = carry if carry.positive?
-
-  head
+  head.next
 end
