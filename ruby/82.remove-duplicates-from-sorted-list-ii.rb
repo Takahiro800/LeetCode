0314aa4abb2d9218d916fb8ac29c7d41c370a4cc
taemath <hirotaka081630@gmail.com>
2024-08-19 21:52:16 +0900

    feat(ruby: 82-remove-duplicates-ii) step1

---
+++ ruby/82.remove-duplicates-from-sorted-list-ii.rb
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def delete_duplicates(head)
+  new_head = ListNode.new(nil, head)
+  prev = new_head
+  target = head
+
+  while target&.next
+    if target.val == target.next.val
+      duplicate_node = skip_duplicates(target)
+      target = duplicate_node
+      prev.next = duplicate_node.next
+    else
+      prev = target
+    end
+
+    target = target.next
+  end
+
+  new_head.next
+end
+
+def skip_duplicates(node)
+  duplicate_node = node
+  while duplicate_node&.next&.val == duplicate_node.val
+    duplicate_node = duplicate_node.next
+  end
+  duplicate_node
+end
