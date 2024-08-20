20fe8038d2adc3c5e33d71e251240869872311bb
taemath <hirotaka081630@gmail.com>
2024-08-20 23:36:05 +0900

    refactor(rust: 82) step2 using match
    hoge

--- rust/src/82.remove-duplicates-from-sorted-list-ii.rs
+++ rust/src/82.remove-duplicates-from-sorted-list-ii.rs
@@ -1,17 +1,4 @@
-use crate::Solution;
-
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>,
-}
-
-impl ListNode {
-    #[inline]
-    fn new(val: i32) -> Self {
-        ListNode { next: None, val }
-    }
-}
+use crate::{remove_duplicates_from_sorted_list::ListNode, Solution};
 
 impl Solution {
     // function nameが被っているので_iiをつけている。提出する際は消すこと
@@ -19,18 +6,21 @@ impl Solution {
         let mut new_head = Some(Box::new(ListNode { val: 0, next: head }));
         let mut prev = new_head.as_mut();
 
-        while let Some(mut current_node) = prev.as_mut().unwrap().next.take() {
-            if let Some(next_node) = current_node.next.as_mut() {
-                if current_node.val == next_node.val {
-                    let duplicate_node = Self::skip_duplicates(current_node);
-                    prev.as_mut().unwrap().next = duplicate_node.next;
-                } else {
-                    prev.as_mut().unwrap().next = Some(current_node);
-                    prev = prev.unwrap().next.as_mut();
+        while let Some(mut target) = prev.as_mut().unwrap().next.take() {
+            match target.next.as_mut() {
+                Some(next_node) => {
+                    if target.val == next_node.val {
+                        let duplicate_node = Self::skip_duplicates(target);
+                        prev.as_mut().unwrap().next = duplicate_node.next;
+                    } else {
+                        prev.as_mut().unwrap().next = Some(target);
+                        prev = prev.unwrap().next.as_mut();
+                    }
                 }
-            } else {
-                prev.as_mut().unwrap().next = Some(current_node);
-                break;
+                None => {
+                    prev.as_mut().unwrap().next = Some(target);
+                    break;
+                }
             }
         }
 
@@ -39,11 +29,11 @@ impl Solution {
 
     fn skip_duplicates(mut node: Box<ListNode>) -> Box<ListNode> {
         while let Some(next_node) = node.next.as_mut() {
-            if next_node.val == node.val {
-                node = node.next.take().unwrap();
-            } else {
+            if next_node.val != node.val {
                 break;
             }
+
+            node = node.next.take().unwrap();
         }
         node
     }
