aa6a9a139ed6e37b850e2e085a24bb1b3d084cf5
taemath <hirotaka081630@gmail.com>
2024-08-20 21:48:37 +0900

    setting modify main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
-}
+#[path = "83.remove-duplicates-from-sorted-list.rs"]
+mod remove_duplicates_from_sorted_list;
+
+#[path = "82.remove-duplicates-from-sorted-list-ii.rs"]
+mod remove_duplicates_from_sorted_list_ii;
+
+fn main() {}
+struct Solution {}
