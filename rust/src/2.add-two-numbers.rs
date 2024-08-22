1f0846fd84f4691cec5d5e339dfbeda4202d6a1e
taemath <hirotaka081630@gmail.com>
2024-08-22 22:06:18 +0900

    feat(rust: 2) step1

--- rust/src/2.add-two-numbers.rs
+++ rust/src/2.add-two-numbers.rs
@@ -1,40 +1,42 @@
-// Definition for singly-linked list.
-// #[derive(PartialEq, Eq, Clone, Debug)]
-// pub struct ListNode {
-//   pub val: i32,
-//   pub next: Option<Box<ListNode>>
-// }
-//
-// impl ListNode {
-//   #[inline]
-//   fn new(val: i32) -> Self {
-//     ListNode {
-//       next: None,
-//       val
-//     }
-//   }
-// }
+use crate::Solution;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+#[allow(dead_code)]
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+#[allow(dead_code)]
 impl Solution {
     pub fn add_two_numbers(
         l1: Option<Box<ListNode>>,
         l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
         let mut head = Some(Box::new(ListNode::new(0)));
-        let (mut l1, mut l2, mut carry, mut current) = (l1, l2, 0, &mut head);
+        let (mut l1, mut l2, mut carry, mut current) = (l1, l2, 0_i32, &mut head);
 
-        while l1.is_some() || l2.is_some() || carry > 0 {
-            let sum = carry + l1.as_ref().map_or(0, |n| n.val) + l2.as_ref().map_or(0, |n| n.val);
-
+        while l1.is_some() || l2.is_some() || carry.is_positive() {
+            let sum = carry
+                + l1.as_ref().map_or(0, |node| node.val)
+                + l2.as_ref().map_or(0, |node| node.val);
             carry = sum / 10;
 
             if let Some(node) = current {
                 node.next = Some(Box::new(ListNode::new(sum % 10)));
                 current = &mut node.next;
             }
-            l1 = l1.and_then(|n| n.next);
-            l2 = l2.and_then(|n| n.next);
+            l1 = l1.and_then(|node| node.next);
+            l2 = l2.and_then(|node| node.next);
         }
 
-        head.unwrap().next
+        head.and_then(|head| head.next)
     }
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,5 +4,8 @@ mod remove_duplicates_from_sorted_list;
 #[path = "82.remove-duplicates-from-sorted-list-ii.rs"]
 mod remove_duplicates_from_sorted_list_ii;
 
+#[path = "2.add-two-numbers.rs"]
+mod add_two_numbers;
+
 fn main() {}
 struct Solution {}
