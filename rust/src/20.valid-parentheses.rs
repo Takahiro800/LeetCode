7d22cc2b1144eeba9e5b96a57119e569fc27bafe
taemath <hirotaka081630@gmail.com>
2024-08-27 09:17:02 +0900

    feat(rust:20) step2

--- rust/src/20.valid-parentheses.rs
+++ rust/src/20.valid-parentheses.rs
@@ -1,29 +1,20 @@
-use std::collections::HashMap;
-//
-// use crate::Solution;
+use crate::Solution;
 
 #[allow(dead_code)]
 impl Solution {
     pub fn is_valid(s: String) -> bool {
-        let bracket_pairs: HashMap<char, char> = Self::get_barcket_pairs();
         let mut stack: Vec<char> = Vec::new();
 
         for char in s.chars() {
-            if bracket_pairs.contains_key(&char) {
-                stack.push(char);
-            } else if Some(&char) != stack.pop().and_then(|c| bracket_pairs.get(&c)) {
-                return false;
+            match char {
+                '(' => stack.push(')'),
+                '[' => stack.push(']'),
+                '{' => stack.push('}'),
+                ')' | ']' | '}' if Some(char) != stack.pop() => return false,
+                _ => (),
             }
         }
 
         stack.is_empty()
-    }
-
-    fn get_barcket_pairs() -> HashMap<char, char> {
-        let mut map = HashMap::new();
-        map.insert('(', ')');
-        map.insert('[', ']');
-        map.insert('{', '}');
-        map
     }
 }
