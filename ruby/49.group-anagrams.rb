e3d90e97a5269af53509a2d6ecbeb970cba0946c
taemath <hirotaka081630@gmail.com>
2024-09-05 21:54:50 +0900

    refactor(ruby:49) step2

--- ruby/49.group-anagrams.rb
+++ ruby/49.group-anagrams.rb
@@ -1,13 +1,5 @@
 # @param {String[]} strs
 # @return {String[][]}
 def group_anagrams(strs)
-  anagrams = Hash.new { |hash, key| hash[key] = [] }
-
-  strs.each do |str|
-    char_count = Hash.new(0)
-    str.each_char { |char| char_count[char] += 1 }
-    anagrams[char_count].push(str)
-  end
-
-  anagrams.values
+  strs.sort.gruoup_by { |str| str.chars.sort }.values
 end
