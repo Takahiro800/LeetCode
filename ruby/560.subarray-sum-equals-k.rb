28f065be12075ab441d4b5126905b3d89243ca27
taemath <hirotaka081630@gmail.com>
2024-09-18 23:34:21 +0900

    refactor(ruby: 560) step2

--- ruby/560.subarray-sum-equals-k.rb
+++ ruby/560.subarray-sum-equals-k.rb
@@ -2,19 +2,14 @@
 # @param {Integer} k
 # @return {Integer}
 def subarray_sum(nums, k)
-  sum = 0
   sum_count = Hash.new(0)
   sum_count[0] = 1
 
-  ans = 0
-
-  nums.each do |num|
+  nums.inject([0, 0]) do |(sum, ans), num|
     sum += num
-    target = sum - k
-    ans += sum_count[target] if sum_count.key?(target)
+    ans += sum_count[sum - k]
 
     sum_count[sum] += 1
-  end
-
-  ans
+    [sum, ans]
+  end.last
 end
