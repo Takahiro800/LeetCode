3baa1e90e4d9dc78ed8bddd69b97fe5a97b54823
taemath <hirotaka081630@gmail.com>
2024-09-18 09:18:53 +0900

    refactor(rust: 1) step2

--- rust/src/1.two-sum.rs
+++ rust/src/1.two-sum.rs
@@ -3,7 +3,6 @@ use std::collections::HashMap;
 use crate::Solution;
 
 #[allow(dead_code)]
-
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut map = HashMap::new();
@@ -11,12 +10,12 @@ impl Solution {
         for (i, num) in nums.iter().enumerate() {
             let rem = target - num;
 
-            if let Some(index) = map.get(&rem) {
-                return vec![*index, i as i32];
-            }
-
-            map.insert(num, i as i32);
+            match map.get(&rem) {
+                Some(index) => return vec![*index, i as i32],
+                None => map.insert(num, i as i32),
+            };
         }
+
         vec![]
     }
 }
