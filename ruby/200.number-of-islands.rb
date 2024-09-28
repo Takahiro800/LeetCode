aaabf8d2296a9e230b713385a3186ac2cdbdda24
taemath <hirotaka081630@gmail.com>
2024-09-28 22:54:03 +0900

    feat(ruby: 200) step1

--- ruby/200.number-of-islands.rb
+++ ruby/200.number-of-islands.rb
@@ -2,7 +2,6 @@
 # @return {Integer}
 def num_islands(grid)
   ans = 0
-  visited = []
   directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
 
   height = grid.size
@@ -10,7 +9,7 @@ def num_islands(grid)
 
   grid.each_with_index do |row, i|
     row.each_with_index do |cell, j|
-      if cell != '1' || visited.include?([i, j])
+      if cell != '1'
         next
       end
 
@@ -20,16 +19,15 @@ def num_islands(grid)
 
       until queue.empty?
         x, y = queue.shift
-        next if visited.include?([x, y])
+        next if grid[x][y] != '1'
 
-        visited << [x, y]
+        grid[y][x] = '0'
 
         directions.each do |dx, dy|
           new_x = x + dx
           new_y = y + dy
           next if new_x >= height || new_y >= width || new_x.negative? || new_y.negative?
           next if grid[new_x][new_y] != '1'
-          next if visited.include?([new_x, new_y])
 
           queue << [new_x, new_y]
         end
