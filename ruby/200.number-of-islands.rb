87fc757caf554901da5a95704f3877d0b093fb66
taemath <hirotaka081630@gmail.com>
2024-09-30 21:54:10 +0900

    refactor(ruby: 200) step1

--- ruby/200.number-of-islands.rb
+++ ruby/200.number-of-islands.rb
@@ -2,38 +2,27 @@
 # @return {Integer}
 def num_islands(grid)
   ans = 0
-  directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
-
-  height = grid.size
-  width = grid[0].size
-
   grid.each_with_index do |row, i|
     row.each_with_index do |cell, j|
       if cell != '1'
         next
       end
 
-      ans += 1
-
-      queue = [[i, j]]
-
-      until queue.empty?
-        x, y = queue.shift
-        next if grid[x][y] != '1'
-
-        grid[y][x] = '0'
-
-        directions.each do |dx, dy|
-          new_x = x + dx
-          new_y = y + dy
-          next if new_x >= height || new_y >= width || new_x.negative? || new_y.negative?
-          next if grid[new_x][new_y] != '1'
-
-          queue << [new_x, new_y]
-        end
-      end
+      ans += dfs(grid, i, j)
     end
   end
 
   ans
+end
+
+def dfs(grid, x, y)
+  return 0 if x.negative? || y.negative? || x >= grid.size || y >= grid[0].size || grid[x][y] != '1'
+
+  grid[x][y] = '0'
+  dfs(grid, x + 1, y)
+  dfs(grid, x - 1, y)
+  dfs(grid, x, y + 1)
+  dfs(grid, x, y - 1)
+
+  1
 end
