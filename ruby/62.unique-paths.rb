03312c300994b2bc257b2e559c2be71d019295c2
taemath <hirotaka081630@gmail.com>
2024-10-20 22:50:29 +0900

    refactor(ruby: 62) step2 by DP

--- ruby/62.unique-paths.rb
+++ ruby/62.unique-paths.rb
@@ -2,9 +2,16 @@
 # @param {Integer} n
 # @return {Integer}
 def unique_paths(m, n)
-  factorial(m + n - 2) / (factorial(m - 1) * factorial(n - 1))
-end
+  prev_row = Array.new(n, 1)
+  current_row = Array.new(n, 1)
 
-def factorial(n)
-  (1..n).inject(:*)
+  (1...m).each do
+    (1...n).each do |j|
+      current_row[j] = prev_row[j] + current_row[j - 1]
+    end
+
+    prev_row = current_row
+  end
+
+  current_row.last
 end
