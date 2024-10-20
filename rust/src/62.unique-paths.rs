f0cfb8e8fee5a7f53197874cbbe938e115ed2948
taemath <hirotaka081630@gmail.com>
2024-10-20 22:50:43 +0900

    refactor(rust: 62) step2 by DP

--- rust/src/62.unique-paths.rs
+++ rust/src/62.unique-paths.rs
@@ -1,11 +1,20 @@
-// use crate::Solution;
+use crate::Solution;
 
 impl Solution {
     pub fn unique_paths(m: i32, n: i32) -> i32 {
-        fn factorial(n: i32) -> i32 {
-            (1..=n).product()
+        let m = m as usize;
+        let n = n as usize;
+
+        let mut prev_row = vec![1_i32; n];
+        let mut current_row = vec![1_i32; n];
+
+        for _ in 1..m {
+            for j in 1..n {
+                current_row[j] = current_row[j - 1] + prev_row[j];
+            }
+            prev_row = current_row.clone();
         }
 
-        factorial(m + n - 2) / (factorial(m - 1) * factorial(n - 1))
+        *prev_row.last().unwrap()
     }
 }
