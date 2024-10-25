fe20e7106750424ea16853c615c5bd251c7460bf
taemath <hirotaka081630@gmail.com>
2024-10-25 23:12:44 +0900

    refactor(ruby: 63) step2

--- ruby/63.unique-paths-ii.rb
+++ ruby/63.unique-paths-ii.rb
@@ -4,22 +4,18 @@ def unique_paths_with_obstacles(obstacle_grid)
   height = obstacle_grid.size
   width = obstacle_grid.first.size
 
-  pash_count = Array.new(height) { Array.new(width, 0) }
-  pash_count[0][0] = obstacle_grid[0][0].zero? ? 1 : 0
+  path_count = Array.new(width, 0)
+  path_count[0] = obstacle_grid[0][0].zero? ? 1 : 0
 
   (0...height).each do |i|
     (0...width).each do |j|
-      next if obstacle_grid[i][j] == 1
-
-      if i.positive? && j.positive?
-        pash_count[i][j] = pash_count[i - 1][j] + pash_count[i][j - 1]
-      elsif i.positive?
-        pash_count[i][j] = pash_count[i - 1][j]
+      if obstacle_grid[i][j] == 1
+        path_count[j] = 0
       elsif j.positive?
-        pash_count[i][j] = pash_count[i][j - 1]
+        path_count[j] += path_count[j - 1]
       end
     end
   end
 
-  pash_count.last.last
+  path_count.last
 end
