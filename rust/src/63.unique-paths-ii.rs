d715ea124573a4764cbcf13afaa14355d0163fc5
taemath <hirotaka081630@gmail.com>
2024-10-26 22:53:09 +0900

    refactor(rust: 63) step2

--- rust/src/63.unique-paths-ii.rs
+++ rust/src/63.unique-paths-ii.rs
@@ -2,7 +2,6 @@ use crate::Solution;
 
 impl Solution {
     pub fn unique_paths_with_obstacles(obstacle_grid: Vec<Vec<i32>>) -> i32 {
-        let height = obstacle_grid.len();
         let width = obstacle_grid[0].len();
 
         let mut path_count: Vec<i32> = vec![0; width];
@@ -10,9 +9,9 @@ impl Solution {
             path_count[0] = 1;
         }
 
-        for i in 0..height {
-            for j in 0..width {
-                if obstacle_grid[i][j] == 1 {
+        for (i, row) in obstacle_grid.iter().enumerate() {
+            for (j, &cell) in row.iter().enumerate() {
+                if cell == 1 {
                     path_count[j] = 0;
                 } else if j > 0 {
                     path_count[j] += path_count[j - 1];
