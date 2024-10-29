e8ffb42a4c05d441c4b67855e2db5a14f64e75d3
taemath <hirotaka081630@gmail.com>
2024-10-29 22:10:21 +0900

    feat(ruby: 121) step2

--- ruby/121.best-time-to-buy-and-sell-stock.rb
+++ ruby/121.best-time-to-buy-and-sell-stock.rb
@@ -1,21 +1,16 @@
 # @param {Integer[]} prices
 # @return {Integer}
 def max_profit(prices)
-  left = 0
-  right = left + 1
+  buy_price = prices[0]
+  max_profit = 0
 
-  current_max = 0
-
-  while right < prices.length
-    diff = prices[right] - prices[left]
-    if diff > 0
-      current_max = diff if diff > current_max
-      right += 1
-    else
-      left = right
-      right = left + 1
+  prices.each do |price|
+    if price < buy_price
+      buy_price = price
+    elsif price - buy_price > max_profit
+      max_profit = price - buy_price
     end
   end
 
-  current_max
+  max_profit
 end
