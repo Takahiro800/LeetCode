af52e680a799b4e3ba5cc377f4b4589777a40e09
taemath <hirotaka081630@gmail.com>
2025-01-22 05:07:22 +0900

    feat(go: 387) step1

---
+++ go/code/387.first-unique-character-in-a-string.go
@@ -0,0 +1,17 @@
+package main
+
+func firstUniqChar(s string) int {
+	counts := make([]int, 26)
+
+	for i := 0; i < len(s); i++ {
+		counts[s[i]-'a']++
+	}
+
+	for i := 0; i < len(s); i++ {
+		if counts[s[i]-'a'] == 1 {
+			return i
+		}
+	}
+
+	return -1
+}
