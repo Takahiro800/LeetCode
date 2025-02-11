777c3ad3f9f89359da7915110f7b0583667fd2b1
taemath <hirotaka081630@gmail.com>
2025-02-11 10:53:27 +0900

    refactor(141: ruby) step2

--- ruby/141.linked-list-cycle.rb
+++ ruby/141.linked-list-cycle.rb
@@ -2,11 +2,11 @@ def hasCycle(head)
   slow = head
   fast = head
 
-  while fast && fast.next
+  while fast&.next&.next
     slow = slow.next
     fast = fast.next.next
 
-    return true if fast == slow
+    return true if slow == fast
   end
 
   false
