c3ed3b62a33d29cf71fad4de2c8eeb0870b0638b
taemath <hirotaka081630@gmail.com>
2025-02-11 16:42:13 +0900

    feat(ruby: 2) step1

--- ruby/2.add-two-numbers.rb
+++ ruby/2.add-two-numbers.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
 def add_two_numbers(l1, l2)
   head = ListNode.new(0)
   current = head
   carry = 0
 
   while l1 || l2 || carry.positive?
-    sum = (l1&.val || 0) + (l2&.val || 0) + carry
+    sum = l1&.val.to_i + l2&.val.to_i + carry
+    rem = sum % 10
     carry = sum / 10
-    current.next = ListNode.new(sum % 10)
-    current = current.next
 
+    current.next = ListNode.new(rem)
+    current = current.next
     l1 = l1&.next
     l2 = l2&.next
   end
