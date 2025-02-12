173ce7f9ac13e3226adbebf0d3e6aa557636f30f
taemath <hirotaka081630@gmail.com>
2025-02-13 08:38:52 +0900

    feat(ruby: 49) step2

--- ruby/49.group-anagrams.rb
+++ ruby/49.group-anagrams.rb
@@ -1,7 +1,5 @@
 # @param {String[]} strs
 # @return {String[][]}
 def group_anagrams(strs)
-  strs.each_with_object(Hash.new { |hash, key| hash[key] = [] }) do |str, hash|
-    hash[str.chars.sort.join] << str
-  end.values
+  strs.sort.group_by { |str| str.chars.sort }.values
 end
