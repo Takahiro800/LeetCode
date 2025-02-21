02bd1b2f714965120233e3b3de0dd55694cf0e36
taemath <hirotaka081630@gmail.com>
2025-02-21 09:04:44 +0900

    feat(ruby: 560) step2

--- ruby/560.subarray-sum-equals-k.rb
+++ ruby/560.subarray-sum-equals-k.rb
@@ -5,11 +5,12 @@ def subarray_sum(nums, k)
   sum_count = Hash.new(0)
   sum_count[0] = 1
 
-  nums.inject([0, 0]) do |(sum, ans), num|
+  sum = 0
+  nums.reduce(0) do |ans, num|
     sum += num
     ans += sum_count[sum - k]
-
     sum_count[sum] += 1
-    [sum, ans]
-  end.last
+
+    ans
+  end
 end
