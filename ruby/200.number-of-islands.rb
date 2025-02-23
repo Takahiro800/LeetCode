afb60a0dae5a71d28db21af2e219daa3e927b5b6
taemath <hirotaka081630@gmail.com>
2025-02-24 05:02:47 +0900

    feat(ruby: 200) step2

--- ruby/200.number-of-islands.rb
+++ ruby/200.number-of-islands.rb
@@ -4,9 +4,7 @@ def num_islands(grid)
   ans = 0
   grid.each_with_index do |row, i|
     row.each_with_index do |cell, j|
-      if cell != '1'
-        next
-      end
+      next if cell != '1'
 
       ans += dfs(grid, i, j)
     end
@@ -16,13 +14,15 @@ def num_islands(grid)
 end
 
 def dfs(grid, x, y)
-  return 0 if x.negative? || y.negative? || x >= grid.size || y >= grid[0].size || grid[x][y] != '1'
+  return 0 if x.negative? || y.negative?
+  return 0 if x >= grid.size || y >= grid[0].size
+  return 0 if grid[x][y] != '1'
 
   grid[x][y] = '0'
-  dfs(grid, x + 1, y)
   dfs(grid, x - 1, y)
-  dfs(grid, x, y + 1)
+  dfs(grid, x + 1, y)
   dfs(grid, x, y - 1)
+  dfs(grid, x, y + 1)
 
   1
 end
