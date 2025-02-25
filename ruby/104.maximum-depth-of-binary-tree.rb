bffa803a98c2d88d57b237bcb4fdb3a2adf2442a
taemath <hirotaka081630@gmail.com>
2025-02-26 05:04:36 +0900

    feat(ruby: 104) step2

--- ruby/104.maximum-depth-of-binary-tree.rb
+++ ruby/104.maximum-depth-of-binary-tree.rb
@@ -1,12 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode
-#     attr_accessor :val, :left, :right
-#     def initialize(val = 0, left = nil, right = nil)
-#         @val = val
-#         @left = left
-#         @right = right
-#     end
-# end
 # @param {TreeNode} root
 # @return {Integer}
 def max_depth(root)
@@ -16,6 +7,5 @@ end
 def depth(node, current_depth)
   return current_depth if node.nil?
 
-  current_depth += 1
-  [depth(node.left, current_depth), depth(node.right, current_depth)].max
+  [depth(node.left, current_depth + 1), depth(node.right, current_depth + 1)].max
 end
