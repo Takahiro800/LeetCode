88181ef5086d20ccbf1c687b9c415c854a78c3a8
taemath <hirotaka081630@gmail.com>
2025-02-26 05:33:13 +0900

    feat(ruby: 617) step2

--- ruby/617.merge-two-binary-trees.rb
+++ ruby/617.merge-two-binary-trees.rb
@@ -14,9 +14,9 @@ end
 def merge_trees(root1, root2)
   return nil if root1.nil? && root2.nil?
 
-  root = TreeNode.new(root1&.val.to_i + root2&.val.to_i)
-  root.left = merge_trees(root1&.left, root2&.left)
-  root.right = merge_trees(root1&.right, root2&.right)
+  node = TreeNode.new(root1&.val.to_i + root2&.val.to_i)
+  node.left = merge_trees(root1&.left, root2&.left)
+  node.right = merge_trees(root1&.right, root2&.right)
 
-  root
+  node
 end
